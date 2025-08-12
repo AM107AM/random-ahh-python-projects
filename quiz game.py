cc87de8e889a705ab4f5d4b68e72bbb5ddbd408a
AM107AM <amhemara@gmail.com>
2025-08-12 11:22:15 +0300

    Add files via upload

--- quiz game.py
+++ quiz game.py
@@ -1,5 +1,14 @@
 from time import sleep
 
+print("Hello, welcome to the quiz game")
+sleep(1.5)
+print(
+    "First enter the questions and their respective answers then pass the device to your friend"
+)
+sleep(2)
+print("Ready?")
+sleep(1)
+
 while True:
     questions = []
     answers = []
@@ -7,17 +16,13 @@ while True:
     score = 0
     question_asked = 1
     while True:
-        questions.append(
-            input(
-                f"Please put in your question({question_asked}) (p to start playing playing): "
-            )
+        question = input(
+            f"Please put in your question({question_asked}) (p to start playing playing): "
         )
-        if "p" in questions:
-            questions.remove("p")
+
+        if question.lower() == "p" or question == "":
             break
-        if "P" in questions:
-            questions.remove("P")
-            break
+        questions.append(question)
         answers.append(
             input(f"Please select the answer to the question({question_asked}): ")
             .lower()
@@ -26,6 +31,16 @@ while True:
         )
         question_asked += 1
 
+    if questions == []:
+        print("Oh ...")
+        sleep(1)
+        print("I guess you don't wanna play then")
+        sleep(2)
+        print("Goodbye")
+        break
+
+    print("And the first question is:...")
+    sleep(1)
     for question in questions:
         print(question)
         guess = input("your answer is: ").lower().strip().replace(" ", "")
@@ -49,5 +64,13 @@ while True:
     score_percent = score / question_num * 100
     print(f"The score you got is:{score_percent:.2f}%")
     play_again = input("Play again? (Yes or No): ").lower().strip().replace(" ", "")
+    while True:
+        if play_again not in {"yes", "no"}:
+            play_again = input(("please select either (Yes or No)"))
+        else:
+            break
+
     if play_again == "no":
+        print("Well, have a good day")
+        sleep(3)
         break
